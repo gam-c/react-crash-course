66afc3b9f0a124d4eea5f2442aea5815df057c5c
gam-c <stavish@gmail.com>
2023-01-04 15:45:53 -0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,35 @@
-import './App.css';
-import Todo from './components/Todo.jsx'
-import Title from './components/Title.jsx'
-import Modal from './components/Modal.jsx'
-
+import "./App.css";
+import Todo from "./components/Todo.jsx";
+import Title from "./components/Title.jsx";
+import Modal from "./components/Modal.jsx";
 
 function App() {
   return (
     <div>
-     <Title />
-     <div>
-      <input type="text" onChange={(event) =>{
-        console.log(event.target.value)
-      }} />
-      <button>Add Todo</button>
-     </div>
-     <div className='todo__wrapper'>
-      <Todo 
-        title="Finish Frontend Simplified"
-        paragraph="Code along with frontend simplified step by step."
-      /> 
-      <Todo 
-        title="Finish Interview Section"
-        paragraph="Finish every interview question in the nest 6 weeks.."
-      /> 
-      <Todo 
-        title="Land a $100k Job"
-        paragraph="Apply to 100 jobs."
-      /> 
-     </div>
-     <Modal question="Are you sure?"/>
+      <Title />
+      <div>
+        <input
+          type="text"
+          onChange={(event) => {
+            console.log(event.target.value);
+          }}
+        />
+        <button>Add Todo</button>
+      </div>
+      <div className="todo__wrapper">
+        <Todo
+          title="Finish Frontend Simplified"
+          paragraph="Code along with frontend simplified step by step."
+        />
+        <Todo
+          title="Finish Interview Section"
+          paragraph="Finish every interview question in the nest 6 weeks.."
+        />
+        <Todo title="Land a $100k Job" paragraph="Apply to 100 jobs." />
+      </div>
+      <Modal question="Are you sure?" />
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,16 +1,16 @@
-import './Todo.css'
+import "./Todo.css";
 
 function Todo({ title, paragraph }) {
-    function deleteTodo(id) {
-        console.log('deleteTodo()', title)
-    }
-    return (
-        <div className='todo'>
-            <h2>{title}</h2>
-            <p>{paragraph}</p>
-            <button onClick={() => deleteTodo(1)}>Delete</button>
-        </div>
-    )
+  function deleteTodo(id) {
+    console.log("deleteTodo()", title);
+  }
+  return (
+    <div className="todo">
+      <h2>{title}</h2>
+      <p>{paragraph}</p>
+      <button onClick={() => deleteTodo(1)}>Delete</button>
+    </div>
+  );
 }
 
-export default Todo
+export default Todo;
